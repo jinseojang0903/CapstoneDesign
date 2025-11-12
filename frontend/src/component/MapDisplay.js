a9a2d84dfe7c5e02457ade7b36a04d8f1425e6ec
jinseeqang2003 <jinseojang4@gmail.com>
2025-11-12 19:40:48 +0900

    feat: map theme settings(지도설정)

--- frontend/src/component/MapDisplay.js
+++ frontend/src/component/MapDisplay.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; 
 import './MapDisplay.css';
+import { useMapTheme } from '../context/MapThemeContext'; 
 
 function MapDisplay() {
-  const position = [37.5665, 126.9780];
+  const position = [37.5665, 126.9780]; 
+  const { currentTheme } = useMapTheme(); 
 
-  return (
-    <div className="map-display-container">
-      <MapContainer 
-        center={position} 
-        zoom={13} 
-        style={{ height: '100%', width: '100%' }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+  return (
+    <div className="map-display-container">
+      <MapContainer 
+        center={position} 
+        zoom={13} 
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution={currentTheme.attribution}
+          url={currentTheme.url}
+        />
+      </MapContainer>
+    </div>
+  );
 }
 
 export default MapDisplay;
